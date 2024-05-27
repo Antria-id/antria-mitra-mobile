3f00b3dda979fcb3a1c4b564d28e7f503257a024
Noorey Schaultz <ahmadnaufal588@gmail.com>
2024-05-27 21:35:38 +0700

    updaten routes

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -2,6 +2,7 @@ import 'package:antria_mitra_mobile/src/features/auth/presentation/pages/login_p
 import 'package:antria_mitra_mobile/src/features/auth/presentation/pages/register_page.dart';
 import 'package:antria_mitra_mobile/src/features/auth/presentation/pages/reset_password_page.dart';
 import 'package:antria_mitra_mobile/src/features/auth/presentation/pages/splash_page.dart';
+import 'package:antria_mitra_mobile/src/features/home/presentation/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
 class AppRoutes {
@@ -13,6 +14,8 @@ class AppRoutes {
         return _materialRoute(const RegisterPage());
       case '/reset-password':
         return _materialRoute(const ResetPasswordPage());
+      case '/home':
+        return _materialRoute(const HomePages());
       default:
         return _materialRoute(const LoginPage());
     }
