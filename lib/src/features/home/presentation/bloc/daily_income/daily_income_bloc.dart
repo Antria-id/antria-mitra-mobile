a25d2eb15e80d96a4b8ac610226b0848b54b90c1
Noorey Schaultz <ahmadnaufal588@gmail.com>
2024-05-27 21:42:47 +0700

    change import package bloc

--- lib/src/features/home/presentation/bloc/daily_income/daily_income_bloc.dart
+++ lib/src/features/home/presentation/bloc/daily_income/daily_income_bloc.dart
@@ -1,7 +1,8 @@
 import 'package:antria_mitra_mobile/src/core/services/services_locator.dart';
 import 'package:antria_mitra_mobile/src/features/home/data/models/daily_income/daily_income_model.dart';
 import 'package:antria_mitra_mobile/src/features/home/domain/usecases/get_daily_income_usecase.dart';
-import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'daily_income_event.dart';
