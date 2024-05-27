74ec3c7a17c20cd61fbe228e39708e24249a3381
Noorey Schaultz <ahmadnaufal588@gmail.com>
2024-05-27 22:20:02 +0700

    rename widget home pages to home page

--- lib/src/features/home/presentation/pages/home_page.dart
+++ lib/src/features/home/presentation/pages/home_page.dart
@@ -9,8 +9,8 @@ import 'package:antria_mitra_mobile/src/themes/app_text_style.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class HomePages extends StatelessWidget {
-  const HomePages({super.key});
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
