a4907c6d68042fd5c010e86a4d4ccbb82afee8b7
Noorey Schaultz <ahmadnaufal588@gmail.com>
2024-05-27 16:39:09 +0700

    add auth routes pages

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,3 +1,6 @@
+import 'package:antria_mitra_mobile/src/features/auth/presentation/pages/login_page.dart';
+import 'package:antria_mitra_mobile/src/features/auth/presentation/pages/register_page.dart';
+import 'package:antria_mitra_mobile/src/features/auth/presentation/pages/reset_password_page.dart';
 import 'package:antria_mitra_mobile/src/features/auth/presentation/pages/splash_page.dart';
 import 'package:flutter/material.dart';
 
@@ -5,11 +8,13 @@ class AppRoutes {
   static Route<dynamic>? onGenerateRoutes(RouteSettings settings) {
     switch (settings.name) {
       case '/':
-        return _materialRoute(
-          const SplashPage(),
-        );
-      default:
         return _materialRoute(const SplashPage());
+      case '/sign-up':
+        return _materialRoute(const RegisterPage());
+      case '/reset-password':
+        return _materialRoute(const ResetPasswordPage());
+      default:
+        return _materialRoute(const LoginPage());
     }
   }
 
