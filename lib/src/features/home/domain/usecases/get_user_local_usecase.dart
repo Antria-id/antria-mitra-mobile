eb7e7c0a029fccb088f6ef8212b2ea55069256df
Noorey Schaultz <ahmadnaufal588@gmail.com>
2024-05-27 21:33:25 +0700

    add usecases

---
+++ lib/src/features/home/domain/usecases/get_user_local_usecase.dart
@@ -0,0 +1,13 @@
+import 'package:antria_mitra_mobile/src/core/failure/failure.dart';
+import 'package:antria_mitra_mobile/src/core/services/services_locator.dart';
+import 'package:antria_mitra_mobile/src/features/auth/data/models/response/user/user_model.dart';
+import 'package:antria_mitra_mobile/src/features/home/domain/repositories/home_repository.dart';
+import 'package:dartz/dartz.dart';
+
+class GetUserLocalUsecase {
+  Future<Either<Failure, UserModel>> getUserFromLocalStorage() async {
+    final response =
+        await serviceLocator<HomeRepository>().getUserFromLocalStorage();
+    return response;
+  }
+}
