5bfc8bb2bc789ba7b075ed420d28e611b4f5e55f
Noorey Schaultz <ahmadnaufal588@gmail.com>
2024-05-27 19:27:53 +0700

    add login and register auth

---
+++ lib/src/features/auth/domain/usecases/login_usecase.dart
@@ -0,0 +1,22 @@
+import 'package:antria_mitra_mobile/src/core/failure/failure.dart';
+import 'package:antria_mitra_mobile/src/core/services/services_locator.dart';
+import 'package:antria_mitra_mobile/src/core/utils/request.dart';
+import 'package:antria_mitra_mobile/src/features/auth/data/models/request/login/login_request_model.dart';
+import 'package:antria_mitra_mobile/src/features/auth/data/models/response/login/login_reponse_model.dart';
+import 'package:antria_mitra_mobile/src/features/auth/domain/repositories/auth_repository.dart';
+import 'package:dartz/dartz.dart';
+
+class LoginUsecase {
+  Future<Either<Failure, LoginResponse>> loginUser(
+      LoginRequest requestUser) async {
+    final response =
+        await serviceLocator<AuthRepository>().login(requestUser: requestUser);
+    if (response.isRight()) {
+      final remoteUser = response.getOrElse(() => LoginResponse());
+      serviceLocator<Request>().updateAuthorization(
+        remoteUser.accessToken!,
+      );
+    }
+    return response;
+  }
+}
