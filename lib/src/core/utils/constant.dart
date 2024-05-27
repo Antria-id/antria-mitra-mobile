200128768f72842e072c491f77015054e7303101
Noorey Schaultz <ahmadnaufal588@gmail.com>
2024-05-27 16:18:22 +0700

    set up utils

---
+++ lib/src/core/utils/constant.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class APIUrl {
+  static String get baseUrl => dotenv.get('BASEURL');
+  static String get loginUserPath => '/auth/login/mitra';
+  static String get registerUserPath => '/mitra/new';
+  static String getProductPath(int mitraId) => '/produk/mitra/$mitraId';
+  static String getMitraPath(int mitraId) => '/mitra/$mitraId';
+  static String getKaryawanPath(int id) => '/karyawan/$id';
+  static String getPesananPath(int mitraId) => '/pesanan/mitra/$mitraId';
+  static String getPesananByInvoicePath(String invoice) => '/pesanan/$invoice';
+  static String getUlasanPath(int mitraId) => '/reviews/mitra/$mitraId';
+  static String getupdateOrderPath(int id) => '/antrian/$id';
+  static String get imagePath => '/image/';
+}
