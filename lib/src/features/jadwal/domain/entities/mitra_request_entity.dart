d6abf832a849df30c41424457834a68e9cb5d2b9
Noorey Schaultz <ahmadnaufal588@gmail.com>
2024-05-28 08:58:28 +0700

    add jadwal entitt

---
+++ lib/src/features/jadwal/domain/entities/mitra_request_entity.dart
@@ -0,0 +1,11 @@
+class MitraRequestEntity {
+  final String? hariBuka;
+  final String? jamBuka;
+  final String? jamTutup;
+
+  MitraRequestEntity({
+    this.hariBuka,
+    this.jamBuka,
+    this.jamTutup,
+  });
+}
