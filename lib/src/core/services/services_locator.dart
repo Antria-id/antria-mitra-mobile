e6a4c5e249d2a1ac905f1c4db8896c41498c7d53
Noorey Schaultz <ahmadnaufal588@gmail.com>
2024-05-29 08:26:08 +0700

    add transaksi services

--- lib/src/core/services/services_locator.dart
+++ lib/src/core/services/services_locator.dart
@@ -5,6 +5,11 @@ import 'package:antria_mitra_mobile/src/features/auth/data/repositories/auth_rep
 import 'package:antria_mitra_mobile/src/features/auth/domain/repositories/auth_repository.dart';
 import 'package:antria_mitra_mobile/src/features/auth/domain/usecases/login_usecase.dart';
 import 'package:antria_mitra_mobile/src/features/auth/domain/usecases/register_usecase.dart';
+import 'package:antria_mitra_mobile/src/features/history_transaksi/data/datasources/riwayat_transaksi_remote_datasource.dart';
+import 'package:antria_mitra_mobile/src/features/history_transaksi/data/repositories/riwayat_transaksi_repository_impl.dart';
+import 'package:antria_mitra_mobile/src/features/history_transaksi/domain/repositories/riwayat_transaksi_repository.dart';
+import 'package:antria_mitra_mobile/src/features/history_transaksi/domain/usecases/get_detail_transaksi_usecase.dart';
+import 'package:antria_mitra_mobile/src/features/history_transaksi/domain/usecases/get_riwayat_usecase.dart';
 import 'package:antria_mitra_mobile/src/features/home/data/datasources/local/home_local_datasource.dart';
 import 'package:antria_mitra_mobile/src/features/home/data/datasources/remote/home_remote_datasource.dart';
 import 'package:antria_mitra_mobile/src/features/home/data/repositories/home_repository_impl.dart';
@@ -58,6 +63,22 @@ Future<void> setUpServiceLocator() async {
   //repository
   serviceLocator.registerFactory<HomeRepository>(() => HomeRepositoryImpl());
   serviceLocator.registerSingleton<UserCacheService>(UserCacheService());
+
+  //transaksi
+
+  //datasource
+  serviceLocator.registerFactory<RiwayatTransaksiRemoteDatasource>(
+      () => RiwayatTransaksiRemoteDatasourceImpl());
+
+  //usecase
+  serviceLocator.registerFactory<GetDetailTransaksiUsecase>(
+      () => GetDetailTransaksiUsecase());
+  serviceLocator.registerFactory<GetRiwayatTransaksiUsecase>(
+      () => GetRiwayatTransaksiUsecase());
+
+  //repository
+  serviceLocator.registerFactory<RiwayatTransaksiRepository>(
+      () => RiwayatTransaksiRepositoryImpl());
 
   //external
   final sharedPreferences = await SharedPreferences.getInstance();
