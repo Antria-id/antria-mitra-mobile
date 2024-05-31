c12610362e331006c451feb346cbaea2f50fa90e
Noorey Schaultz <ahmadnaufal588@gmail.com>
2024-06-01 06:54:37 +0700

    update antrian feat

--- lib/src/features/antrian/presentation/widgets/tabs_list.dart
+++ lib/src/features/antrian/presentation/widgets/tabs_list.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 
 class Tabs {
   final List<Tab> tabs = [
-    const Tab(text: 'Pending'),
-    const Tab(text: 'Antrian'),
-    const Tab(text: 'Pengambilan'),
+    const Tab(text: 'Menunggu'),
+    const Tab(text: 'Diproses'),
+    const Tab(text: 'Selesai'),
   ];
 }
