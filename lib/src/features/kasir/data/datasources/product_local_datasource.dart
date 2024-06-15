0a3feb31a2064d91b8a3fe9c6eb3d7743b25f544
Noorey Schaultz <ahmadnaufal588@gmail.com>
2024-06-15 15:09:54 +0700

    add local datasource

---
+++ lib/src/features/kasir/data/datasources/product_local_datasource.dart
@@ -0,0 +1,104 @@
+import 'package:antria_mitra_mobile/src/core/failure/failure.dart';
+import 'package:antria_mitra_mobile/src/core/helper/database_helper.dart';
+import 'package:antria_mitra_mobile/src/features/kasir/data/models/product_model.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class ProductLocalDatasource {
+  Future<Either<Failure, List<ProductModel>>> getAllProducts();
+  Future<Either<Failure, void>> addProductToOrderList(
+      int productId, int quantity);
+  Future<Either<Failure, List<Map<String, dynamic>>>> getProductsInOrderList();
+  Future<Either<Failure, void>> incrementOrderQuantity(
+      int productId, int quantity);
+  Future<Either<Failure, void>> decrementOrderQuantity(
+      int productId, int quantity);
+}
+
+class ProductLocalDatasourceImpl implements ProductLocalDatasource {
+  @override
+  Future<Either<Failure, List<ProductModel>>> getAllProducts() async {
+    try {
+      final DatabaseHelper databaseHelper = DatabaseHelper.instance;
+      final result = await databaseHelper.getAllProduct();
+      if (result.isNotEmpty) {
+        final List<ProductModel> products = result.map<ProductModel>((json) {
+          return ProductModel.fromJson(json);
+        }).toList();
+        return Right(products);
+      } else {
+        return const Left(LocalDatabaseQueryFailure('No products found'));
+      }
+    } catch (e) {
+      return Left(LocalDatabaseQueryFailure('Unable to retrieve products: $e'));
+    }
+  }
+
+  @override
+  Future<Either<Failure, void>> addProductToOrderList(
+      int productId, int quantity) async {
+    try {
+      final DatabaseHelper databaseHelper = DatabaseHelper.instance;
+      final DateTime now = DateTime.now();
+
+      // Add product to orderList
+      await databaseHelper.addOrderList(
+        productId,
+        quantity,
+        now,
+        now,
+      );
+
+      return const Right(null);
+    } catch (e) {
+      return Left(
+        LocalDatabaseQueryFailure(
+          'Unable add product to orderList: $e',
+        ),
+      );
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<Map<String, dynamic>>>>
+      getProductsInOrderList() async {
+    try {
+      final DatabaseHelper databaseHelper = DatabaseHelper.instance;
+      final result = await databaseHelper.getProductsInOrderList();
+      if (result.isNotEmpty) {
+        return Right(result);
+      } else {
+        return const Left(
+            LocalDatabaseQueryFailure('No products in order list found'));
+      }
+    } catch (e) {
+      return Left(LocalDatabaseQueryFailure(
+          'Unable to retrieve products in order list: $e'));
+    }
+  }
+
+  @override
+  Future<Either<Failure, void>> incrementOrderQuantity(
+      int productId, int quantity) async {
+    try {
+      final DatabaseHelper databaseHelper = DatabaseHelper.instance;
+      await databaseHelper.incrementOrderQuantity(productId, quantity);
+      return const Right(null);
+    } catch (e) {
+      return Left(
+          LocalDatabaseQueryFailure('Unable to increment order quantity: $e'));
+    }
+  }
+
+  @override
+  Future<Either<Failure, void>> decrementOrderQuantity(
+      int productId, int quantity) async {
+    try {
+      final DatabaseHelper databaseHelper = DatabaseHelper.instance;
+      await databaseHelper.decrementOrderQuantity(productId, quantity);
+      return const Right(null);
+    } catch (e) {
+      return Left(
+          LocalDatabaseQueryFailure('Unable to decrement order quantity: $e'));
+    }
+  }
+}
